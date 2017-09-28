83049a719606c8a51931cd6673812b8902282c58
DFROBOT xiao wo <30491648+forKnowYou@users.noreply.github.com>
2017-09-28 15:35:54 +0800

    format

--- DFRobot_BME680_I2C.cpp
+++ DFRobot_BME680_I2C.cpp
@@ -11,13 +11,13 @@ void DFRobot_BME680_I2C::readReg(uint8_t addr, uint8_t count, uint8_t* pBuf)
 {
   Wire.begin();
   Wire.beginTransmission(this->I2C_addr);
-	Wire.write(addr);
-	Wire.endTransmission();
-	Wire.requestFrom(this->I2C_addr, count);
-	while(Wire.available()) {
-		*pBuf = Wire.read();
-		pBuf ++;
-	}
+  Wire.write(addr);
+  Wire.endTransmission();
+  Wire.requestFrom(this->I2C_addr, count);
+  while(Wire.available()) {
+    *pBuf = Wire.read();
+    pBuf ++;
+  }
 }
 
 
@@ -25,9 +25,9 @@ void DFRobot_BME680_I2C::writeReg(uint8_t addr, uint8_t dat)
 {
   Wire.begin();
   Wire.beginTransmission(this->I2C_addr);
-	Wire.write(addr);
-	Wire.write(dat);
-	Wire.endTransmission();
+  Wire.write(addr);
+  Wire.write(dat);
+  Wire.endTransmission();
 }
 
 
@@ -35,8 +35,8 @@ uint8_t DFRobot_BME680_I2C::readID(void)
 {
   uint8_t       id = 0;
   this->writeReg(BME680_RESET, 0xb6);
-	delay(100);
-	this->readReg(BME680_ID, 1, &id);
+  delay(100);
+  this->readReg(BME680_ID, 1, &id);
   Serial.print("ID is :0x");
   Serial.println(id, HEX);
   return id;
